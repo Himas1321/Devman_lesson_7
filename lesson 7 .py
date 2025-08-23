8b9da2c1d7b5ba0af81bf928b3e5049132f1c35b
Himas1321 <nikita5045@mail.ru>
2025-08-23 11:54:33 +0900

    Update lesson 7 .py

--- lesson 7 .py
+++ lesson 7 .py
@@ -12,21 +12,25 @@ TG_CHAT_ID = os.getenv('TG_CHAT_ID')
 
 def wait(chat_id, question):
     parsing = parse(question)
-    bot.create_timer(parsing, choose, author_id=chat_id, message=question)
-    bot.create_countdown(parsing, notify_progress, secs_left=chat_id)
+    bot.create_countdown(parsing, notify_progress, author_id=chat_id)
+    bot.create_timer(parsing, choose, chat_id=chat_id)
+    
 
 
-def notify_progress(secs_left):
-    bot.send_message("Осталось секунд:", secs_left)
+def notify_progress(secs_left, author_id):
+    bot.send_message(author_id, "Осталось {} секунд!".format(secs_left))
 
 
-def choose(author_id, message):
-    bot.send_message(author_id, 'Время вышло')
+def choose(chat_id):
+    bot.send_message(chat_id, 'Время вышло')
+
+
 
 
 bot = ptbot.Bot(TG_TOKEN)
 bot.reply_on_message(wait)
 bot.run_bot()
+
 
 
 
