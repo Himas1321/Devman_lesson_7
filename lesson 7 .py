bc42bbd58daf769cc19dafc5567060aa3c303422
Himas1321 <nikita5045@mail.ru>
2025-08-24 15:16:19 +0900

    Update lesson 7 .py

--- lesson 7 .py
+++ lesson 7 .py
@@ -9,28 +9,40 @@ load_dotenv()
 TG_TOKEN = os.getenv('TG_TOKEN') 
 TG_CHAT_ID = os.getenv('TG_CHAT_ID')
 
+bot = ptbot.Bot(TG_TOKEN)
+
 
 def wait(chat_id, question):
     parsing = parse(question)
-    bot.create_countdown(parsing, notify_progress, author_id=chat_id)
-    message_id = bot.send_message(TG_CHAT_ID, "Моё сообщение")
-    bot.create_timer(parsing, choose, author_id=chat_id)
-    print('ID сообщения', message_id)
+    message_id = bot.send_message(chat_id, "Моё сообщение")
+    bot.create_countdown(parsing, notify_progress,
+                        chat_id=chat_id, message_id=message_id, parsing=parsing)
+    bot.create_timer(parsing, choose, chat_id=chat_id)
 
 
-def notify_progress(secs_left, author_id):
-    # message_id = bot.send_message(author_id, "Осталось {} секунд!".format(secs_left))
-    bot.update_message(author_id, message_id, "Осталось {} секунд!".format(secs_left))
+def notify_progress(secs_left, chat_id, message_id, parsing):
+    bot.update_message(chat_id, message_id, "Осталось {} секунд!".format(secs_left))
+    progressbar = render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░')
 
 
-
-def choose(author_id):
-    bot.send_message(author_id, 'Время вышло')
-
+def choose(chat_id):
+    bot.send_message(chat_id, 'Время вышло')
 
 
+def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
+    iteration = min(total, iteration)
+    percent = "{0:.1f}"
+    percent = percent.format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    pbar = fill * filled_length + zfill * (length - filled_length)
+    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
-bot = ptbot.Bot(TG_TOKEN)
-bot.reply_on_message(wait)
-bot.run_bot()
+
+def main():
+    bot.reply_on_message(wait)
+    bot.run_bot()
+
+
+if __name__ == '__main__':
+    main()
 
