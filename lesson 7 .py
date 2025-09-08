ee5917490fb04f8cdffc84ce0dcb4a87f1076884
Himas1321 <nikita5045@mail.ru>
2025-09-08 18:56:10 +0900

    Update lesson 7 .py

--- lesson 7 .py
+++ lesson 7 .py
@@ -5,19 +5,28 @@ import random
 from pytimeparse import parse
 
 
-def reply(chat_id, question):
+def main():
+    load_dotenv()
+    tg_token = os.getenv('TG_TOKEN')
+    tg_chat_id = os.getenv('TG_CHAT_ID')
+    bot = ptbot.Bot(tg_token)
+    bot.reply_on_message(reply, bot=bot)
+    bot.run_bot()
+   
+def reply(chat_id, question, **bot):
+    
     parsing = parse(question)
     message_id = bot.send_message(chat_id, "Запуск таймера")
-    bot.create_countdown(parsing, notify, chat_id=chat_id, 
+    bot.create_countdown(bot, parsing, notify, chat_id=chat_id, 
                          message_id=message_id, parsing=parsing)
     bot.create_timer(parsing, choose, chat_id=chat_id)
 
 
-def notify(secs_left, chat_id, message_id, parsing):
+def notify(secs_left, chat_id, message_id, parsing,**bot):
     bot.update_message(chat_id, message_id, "Осталось {} секунд! \n {}" .format(secs_left, render_progressbar(parsing, secs_left )))
     
 
-def choose(chat_id):
+def choose(chat_id, **bot):
     bot.send_message(chat_id, 'Время вышло')
 
 
@@ -30,13 +39,8 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-if __name__ == '__main__':
-    load_dotenv()
-    tg_token = os.getenv('TG_TOKEN') 
-    tg_chat_id = os.getenv('TG_CHAT_ID')
-    bot = ptbot.Bot(tg_token)
-    bot.reply_on_message(reply)
-    bot.run_bot()
 
+if __name__ == '__main__':
+    main()
 
 
